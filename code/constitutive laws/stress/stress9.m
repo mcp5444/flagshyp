5b8ef281b7e1c357418b5502dedffedcd7834812
mcp5444 <165069588+mcp5444@users.noreply.github.com>
2024-03-26 17:09:21 -0400

    Update stress9.m

--- code/constitutive laws/stress/stress9.m
+++ code/constitutive laws/stress/stress9.m
@@ -13,7 +13,7 @@ k = lambda+2*mu/3;
 k/mu;
 C               = kinematics.F'*kinematics.F;
 C_bar           = (J^(-2/3)*C);
-S               = J^(-2/3)*((-1/3)*((mu*cons.I+2*mu*cons.I)*C_bar^-1+(mu+mu*cons.I)*cons.I-mu*C_bar));
+S               = J^(-2/3) * (((-1/3) * (mu * cons.I + 2 * mu * cons.I) * (C_bar^-1)) + ((mu + mu * cons.I) * cons.I) - (mu * C_bar));
 Cauchy          = kinematics.F*S*kinematics.F';
 
 end
