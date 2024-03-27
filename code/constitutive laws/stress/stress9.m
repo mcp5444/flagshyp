b42b0408f4810f935283e437152fa6fec9178ec4
mcp5444 <165069588+mcp5444@users.noreply.github.com>
2024-03-27 18:00:52 -0400

    Update stress9.m

--- code/constitutive laws/stress/stress9.m
+++ code/constitutive laws/stress/stress9.m
@@ -2,18 +2,21 @@
 % Evaluates the Cauchy stress tensor for material type 8.
 %--------------------------------------------------------------------------
 function Cauchy = stress9(kinematics,properties,cons)
-mu              = properties(2);
+mu1              = 0.595522;
+mu2              = 0.050381;
 lambda          = properties(3);
 J               = kinematics.J;
 b               = kinematics.b;
+k = 1e5;
 
 % Additions for Mooney Rivilin
 
-k = lambda+2*mu/3;
-k/mu;
 C               = kinematics.F'*kinematics.F;
 C_bar           = (J^(-2/3)*C);
-S               = J^(-2/3) * (((-1/3) * (mu * cons.I + 2 * mu * cons.I) * (C_bar^-1)) + ((mu + mu * cons.I) * cons.I) - (mu * C_bar));
+
+Siso            = J^(-2/3) * (((-1/3) * (mu1 * cons.I + 2 * mu2 * cons.I) * (C_bar^-1)) + ((mu1 + mu2 * cons.I) * cons.I) - (mu2 * C_bar));
+Svol            = (J^(1/3)) * (J - 1) * k * (C_bar^-1)
+S               = Siso + Svol
 Cauchy          = kinematics.F*S*kinematics.F';
 
 end
